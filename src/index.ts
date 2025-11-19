8558b26194449d5dd5d1462b67e3c9449e02f873
Chace Daniels <chace.daniels@outsystems.com>
2025-11-19 08:54:06 -0600

    chore: update npm packages

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { registerPlugin } from '@capacitor/core';
 import type { HapticsPlugin } from './definitions';
 
 const Haptics = registerPlugin<HapticsPlugin>('Haptics', {
-  web: () => import('./web').then(m => new m.HapticsWeb()),
+  web: () => import('./web').then((m) => new m.HapticsWeb()),
 });
 
 export * from './definitions';
